5f55158a412267d53b215d9560d304308da28d8f
Rafael Colin <faelnek@gmail.com>
2023-08-24 12:15:51 -0300

    Initializing the Spring boot project, creating an enterprise system

---
+++ src/main/java/com/colin/springboot/springemployeers/SpringEmployeersApplication.java
@@ -0,0 +1,13 @@
+package com.colin.springboot.springemployeers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmployeersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colin/springboot/springemployeers/SpringEmployeersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colin.springboot.springemployeers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
